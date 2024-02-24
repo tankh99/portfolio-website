0e6d0dbddc00926c7f0682747f79d4bfc301744b
tankh99 <khanghou99@gmail.com>
2024-02-24 22:02:58 +0800

    chore: fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Khang Hou | Personal Portfolio",
+  description: "Khang Hou is a fool-scap developer.",
 };
 
 export default function RootLayout({
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; 2099 Khang Hou. All rights reserved, until government asks me to return the rights.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
